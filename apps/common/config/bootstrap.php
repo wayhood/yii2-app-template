357b807eb14adc1af12ece7236d14e19b9cea1d8
netyum <netyum@163.com>
2015-01-20 23:26:13 +0800

    Fix apps/common/config/bootstrap.php

--- apps/common/config/bootstrap.php
+++ apps/common/config/bootstrap.php
@@ -1,17 +1,3 @@
 <?php
 Yii::setAlias('common', dirname(__DIR__));
-Yii::setAlias('frontend', dirname(dirname(__DIR__)) . '/frontend');
-Yii::setAlias('backend', dirname(dirname(__DIR__)) . '/backend');
-Yii::setAlias('console', dirname(dirname(__DIR__)) . '/console');
 
-Yii::setAlias('abc', dirname(dirname(__DIR__)) . '/abc');
-
-Yii::setAlias('merchant', dirname(dirname(__DIR__)) . '/merchant');
-
-Yii::setAlias('management', dirname(dirname(__DIR__)) . '/management');
-
-Yii::setAlias('a', dirname(dirname(__DIR__)) . '/a');
-
-Yii::setAlias('b', dirname(dirname(__DIR__)) . '/b');
-
-Yii::setAlias('mygod', dirname(dirname(__DIR__)) . '/mygod');
